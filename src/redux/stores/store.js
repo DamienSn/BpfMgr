9383035c3172bd7d1d8e14daef151264b4fac089
DamienSn <damienstephan07@gmail.com>
2021-10-14 21:01:08 +0200

    update store with the new reducers

--- src/redux/stores/store.js
+++ src/redux/stores/store.js
@@ -2,10 +2,14 @@ import { combineReducers, createStore, applyMiddleware } from "redux"
 import userReducer from "../reducers/user.reducer";
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
+import dptsReducer from "../reducers/dpts.reducer";
+import provincesReducer from "../reducers/provinces.reducer";
 
 const store = createStore(
     combineReducers({
         user: userReducer,
+        dpts: dptsReducer,
+        provinces: provincesReducer
     }),
     composeWithDevTools(applyMiddleware(thunk))
 )
