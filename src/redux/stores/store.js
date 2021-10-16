04585553c1c497ef35bd5efcc50222e33f20e718
DamienSn <damienstephan07@gmail.com>
2021-10-16 22:51:02 +0200

    update redux store

--- src/redux/stores/store.js
+++ src/redux/stores/store.js
@@ -5,13 +5,17 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 import dptsReducer from "../reducers/dpts.reducer";
 import provincesReducer from "../reducers/provinces.reducer";
 import citiesReducer from "../reducers/cities.reducer";
+import paneReducer from "../reducers/pane.reducer";
+import logReducer from "../reducers/log.reducer";
 
 const store = createStore(
     combineReducers({
         user: userReducer,
         dpts: dptsReducer,
         provinces: provincesReducer,
-        cities: citiesReducer
+        cities: citiesReducer,
+        mapPane: paneReducer,
+        logModal: logReducer
     }),
     composeWithDevTools(applyMiddleware(thunk))
 )
