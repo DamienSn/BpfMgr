61f73f33cc2c08eea8430cc0a47bf09c35f99f3d
DamienSn <damienstephan07@gmail.com>
2021-11-14 19:15:15 +0100

    authorize x-api-key header

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 3000;
 const corsOptions = {
     origin: process.env.CLIENT_URL,
     credentials: true,
-    allowedHeaders: ["sessionId", "Content-Type"],
+    allowedHeaders: ["sessionId", "Content-Type", 'x-api-key'],
     exposedHeaders: ["sessionId"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
