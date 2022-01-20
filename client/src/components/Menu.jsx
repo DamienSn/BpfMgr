15f4435733e63808d277b64badcb949049a2b2f0
DamienSn <damienstephan07@gmail.com>
2022-01-20 21:40:29 +0100

    menu tooltips

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,8 +1,25 @@
 import { HomeIcon, PlusCircleIcon, CollectionIcon, MapIcon, SearchIcon } from '@heroicons/react/outline';
 import { Link } from 'react-router-dom'
 import { useEffect } from 'react';
+import React from 'react';
 
 export default function Menu() {
+    return (
+        <aside className="menu">
+            <nav>
+                <ul>
+                    <MenuItem text="Accueil" path="/"><HomeIcon className='icon-md' /></MenuItem>
+                    <MenuItem text="Ajouter un BPF/BCN" path="/add"><PlusCircleIcon className='icon-md' /></MenuItem>
+                    <MenuItem text="Mes BPF/BCN" path="/list"><CollectionIcon className='icon-md' /></MenuItem>
+                    <MenuItem text="Carte" path="/map"><MapIcon className='icon-md' /></MenuItem>
+                    <MenuItem text="Recherche" path="/search"><SearchIcon className='icon-md' /></MenuItem>
+                </ul>
+            </nav>
+        </aside>
+    )
+}
+
+function MenuItem(props) {
     const removeActiveLinks = () => {
         const icons = document.querySelectorAll('.nav-link');
         icons.forEach((icon) => {
@@ -44,36 +61,11 @@ export default function Menu() {
     useEffect(activateLinks);
 
     return (
-        <aside className="menu">
-            <nav>
-                <ul>
-                    <li onClick={activateLinks} title="Accueil">
-                        <Link to="/" style={{ textDecoration: 'none', color: '#fff' }} className="nav-link">
-                                <HomeIcon className="icon-md" />
-                        </Link>
-                    </li>
-                    <li onClick={activateLinks} title="Ajouter un BPF/BCN">
-                        <Link to="/add" style={{ textDecoration: 'none', color: '#fff' }} className="nav-link">
-                                <PlusCircleIcon className="icon-md" />
-                        </Link>
-                    </li>
-                    <li onClick={activateLinks} title="Mes BPF/BCN">
-                        <Link to="/list" style={{ textDecoration: 'none', color: '#fff' }} className="nav-link">
-                                <CollectionIcon className="icon-md" />
-                        </Link>
-                    </li>
-                    <li onClick={activateLinks} title="Carte">
-                        <Link to="/map" style={{ textDecoration: 'none', color: '#fff' }} className="nav-link">
-                                <MapIcon className="icon-md" />
-                        </Link>
-                    </li>
-                    <li onClick={activateLinks} title="Recherche">
-                        <Link to="/search" style={{ textDecoration: 'none', color: '#fff' }} className="nav-link">
-                                <SearchIcon className="icon-md" />
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        </aside>
+        <li onClick={activateLinks} title={props.text} className='relative group'>
+            <Link to={props.path} style={{ textDecoration: 'none', color: '#fff' }} className="nav-link">
+                {props.children}
+            </Link>
+            <span className="menu-tooltip group-hover:scale-100">{props.text}</span>
+        </li>
     )
 }
