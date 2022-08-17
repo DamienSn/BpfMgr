9ca533d9a189237b2289d58ec119dbd9fbf70715
DamienSn <damienstephan07@gmail.com>
2022-08-17 12:06:51 +0200

    sync back

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,30 +6,6 @@ const options = {
   mode: process.env.VITE_PWA_MODE,
   base: '/',
   includeAssets: ['favicon.ico', 'logo192.png', 'logo512.png'],
-  strategies: 'injectManifest',
-  manifest: {
-    name: 'BpfMgr',
-    short_name: 'Gestionnaire de BPF et BCN',
-    theme_color: '#ffffff',
-    icons: [
-      {
-        src: 'logo192.png', // <== don't add slash, for testing
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: 'logo512.png', // <== don't remove slash, for testing
-        sizes: '512x512',
-        type: 'image/png',
-      },
-      {
-        src: 'logo512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any maskable',
-      }
-    ],
-  },
 };
 
 // https://vitejs.dev/config/
